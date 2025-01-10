a1553775397d996ab5e831c6d8ae1ac6ff81c62b
L. McKinley Nevins <lmnevins23@gmail.com>
2025-01-09 17:25:18 -0800

    Initial commit

---
+++ trait_explore.R
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------------------------#
+# Exploring WFDP tree root and leaf trait data 
+# Original Author: L. McKinley Nevins 
+# January 9, 2025
+# Software versions:  R v 4.4.1
+#                     tidyverse v 2.0.0
+#                     
+# -----------------------------------------------------------------------------#
+
+# PACKAGES, SCRIPTS, AND SETUP ####
+library(tidyverse); packageVersion("tidyverse")
+
+#################################################################################
+#                               Main workflow                                   #
+#  Explore the leaf and root trait data. Determine if there are differences     #
+#  between species, and maybe try to map some of the variation across the plot? #
+#                                                                               #
+#################################################################################
+
+
+# Load in leaf and root trait datasets 
+
+root <- read.csv()
+
+
+
