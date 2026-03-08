94aec54c21d46f9eca8329df118433c3a0ee0025
L. McKinley Nevins <lmnevins23@gmail.com>
2026-03-08 16:47:12 -0700

    feat: update WA state map

--- WFDP_maps.R
+++ WFDP_maps.R
@@ -41,9 +41,10 @@ map.axes()
 
 # Regional view 
 maps::map(database = 'state', region = c('washington', 'oregon', 'idaho', 'montana'), 
-          boundary = TRUE, xlim=c(-126,-115), ylim=c(45.0,49.0), col = "gray", interior = TRUE, fill=TRUE)
+          boundary = TRUE, xlim=c(-125,-116), ylim=c(45.0,49.0), col = "gray", interior = TRUE, fill=TRUE)
 points(-121.957111, 45.819740, pch = 19, col = 'red3', cex = 1.5)
-map.axes()
+map.axes() 
+map.scale(x = -119.5, y = 45.5, ratio = FALSE)
 
 # Counties - probably not relevant 
 maps::map('county', 'washington', boundary = TRUE, xlim=c(-124,-120), ylim=c(45.3,47.0), 
